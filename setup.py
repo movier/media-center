6ce06e53db270da0811fa2ebe0009e7905455763
Oliver <olimovier@gmail.com>
2019-05-17 17:21:45 +0800

    Traverse a directory and insert data

--- setup.py
+++ setup.py
@@ -2,15 +2,31 @@ from sqlalchemy import create_engine
 from models import Base, Video
 from sqlalchemy.orm import sessionmaker
 from db import engine, SessionLocal
+from os import listdir
+from os.path import isfile, join, splitext
 
 Base.metadata.create_all(bind=engine)
 
 db_session = SessionLocal()
 
-first_video = db_session.query(Video).first()
-if not first_video:
-    v = Video(title="sample-video", uri="folder/sample-video.mp4", poster_uri="folder/sample-video.jpg")
-    db_session.add(v)
-    db_session.commit()
+mypath = "/usr/src/app"
+
+def traverse_dir(base_path):
+    for f in listdir(base_path):
+        path = join(base_path, f)
+        if isfile(path):
+            title, ext = splitext(f)
+            if ext == ".py":
+                title = "".join(title)
+                uri = path[len(mypath):]
+                root, ext1 = splitext(uri)
+                poster_uri = "".join(root) + ".jpg"
+                v = Video(title=title, uri=uri, poster_uri=poster_uri)
+                db_session.add(v)
+        else:
+            traverse_dir(path)
+
+traverse_dir(mypath)
+db_session.commit()
 
 db_session.close()
