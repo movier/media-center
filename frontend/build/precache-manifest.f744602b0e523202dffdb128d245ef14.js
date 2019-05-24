fa8a5fc3fc8ff55d6d11c0d5d36da4b1750d4a11
Oliver <olimovier@gmail.com>
2019-05-24 14:18:06 +0800

    1st deployment of frontend

---
+++ frontend/build/precache-manifest.f744602b0e523202dffdb128d245ef14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37ef706265da051e857c3b1303b8cd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a47864978fdc246ff25",
+    "url": "/static/css/main.047ae6f3.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5a47864978fdc246ff25",
+    "url": "/static/js/main.e77f2028.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
