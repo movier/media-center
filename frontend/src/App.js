bb833d2b9d66c203568e6bb166fcc215572fa746
Oliver <olimovier@gmail.com>
2019-05-24 14:15:56 +0800

    Testing api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  const data = [];
-  for (let index = 0; index < 103; index++) {
-    data.push(index);
+class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost/api');
   }
-  return (
-    <div className="App">
-      {data.map((value, index) => {
-        return (
-          <div key={index}>
-            <img src="/images/placeholder.png" alt={index} />
-            <p>This is description of the image</p>
-          </div>
-        );
-      })}
-    </div>
-  );
+
+  render() {
+    const data = [];
+    for (let index = 0; index < 103; index++) {
+      data.push(index);
+    }
+    return (
+      <div className="App">
+        {data.map((value, index) => {
+          return (
+            <div key={index}>
+              <img src="/images/placeholder.png" alt={index} />
+              <p>This is description of the image</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
