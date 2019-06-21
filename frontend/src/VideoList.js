4e1ecf0d298fd253fe0e90485fb30cc80e4318e1
Oliver <olimovier@gmail.com>
2019-06-21 17:06:07 +0800

    Check update through a button

--- frontend/src/VideoList.js
+++ frontend/src/VideoList.js
@@ -19,6 +19,9 @@ class VideoList extends React.Component {
         console.log('response', data);
         this.setState({ data });
       });
+  }
+
+  handleCheckUpdate = () => {
     fetch('/api?is_check=true')
       .then(response => response.json())
       .then(data => {
@@ -28,17 +31,22 @@ class VideoList extends React.Component {
 
   render() {
     return (
-      <div className="VideoList">
-        {this.state.data.map((value, index) => {
-          return (
-            <div key={index}>
-              <Link className="VideoList__link" to={`/watch?v=${value.uri}`}>
-                <img src={value.poster_uri} alt={value.title} />
-                <p className="VideoList__title">{value.title}</p>
-              </Link>
-            </div>
-          );
-        })}
+      <div>
+        <div style={{ marginTop: 16, textAlign: 'center' }}>
+          <button onClick={this.handleCheckUpdate}>检查更新</button>
+        </div>
+        <div className="VideoList">
+          {this.state.data.map((value, index) => {
+            return (
+              <div key={index}>
+                <Link className="VideoList__link" to={`/watch?v=${value.uri}`}>
+                  <img src={value.poster_uri} alt={value.title} />
+                  <p className="VideoList__title">{value.title}</p>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
