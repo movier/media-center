31048cf074c0b10331f1ccc9aa287d0d0d92b85e
Oliver <olimovier@gmail.com>
2020-01-24 14:52:05 +0800

    Fix a bug

--- react/src/CastList.js
+++ react/src/CastList.js
@@ -30,10 +30,11 @@ class CastList extends React.Component {
               <div style={{ color: 'white' }}>{name}</div>
               <div className="VideoList">
                 {videos.map((value, index) => {
-                  const { uri, poster_uri, title, id } = value;
+                  const { uri, poster_uri, title, id, cast } = value;
+                  const castNames = cast.map(m => m.name).join(',');
                   return (
                     <div key={index}>
-                      <Link className="VideoList__link" to={`/watch?v=${uri}&id=${id}`}>
+                      <Link className="VideoList__link" to={`/watch?v=${uri}&id=${id}&cast=${castNames}`}>
                         <img src={poster_uri} alt={title} />
                         <p className="VideoList__title">{title}</p>
                       </Link>
