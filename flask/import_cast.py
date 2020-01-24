8c19b4a2d32132b46941d47834d86dedd90c59d6
Oliver <olimovier@gmail.com>
2020-01-24 13:59:26 +0800

    Import cast script

--- flask/import_cast.py
+++ flask/import_cast.py
@@ -1,8 +1,17 @@
 from manage import db, Video, Cast
 
-video = Video.query.get(2)
-cast = Cast(name='Test2')
-video.cast.append(cast)
-db.session.add(video)
-db.session.commit()
+listOfNonCastSubPath = ['others', 'unsolved', 'newdata', 'new']
+for row in Video.query.all():
+  sub_path = row.uri.split('/')
+  print(sub_path)
+  if len(sub_path) > 2 and sub_path[1] not in listOfNonCastSubPath:
+    cast_name = sub_path[1]
+    query_cast = Cast.query.filter_by(name=cast_name)
+    if query_cast.count() > 0:
+      c = query_cast.first()
+    else:
+      c = Cast(name=cast_name)
+    row.cast.append(c)
+    db.session.add(row)
 
+db.session.commit()
