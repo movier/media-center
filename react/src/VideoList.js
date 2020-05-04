6c459dd042b37adf1415afdc8776a89e8e803be2
Oliver <olimovier@gmail.com>
2020-05-04 17:46:39 +0800

    New FE versions

--- react/src/VideoList.js
+++ react/src/VideoList.js
@@ -9,6 +9,7 @@ class VideoList extends React.Component {
     
     this.state = {
       data: [],
+      test: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     };
   }
 
@@ -55,6 +56,11 @@ class VideoList extends React.Component {
               </div>
             );
           })}
+          {this.state.test.map(value => (
+            <div key={value} style={{ width: '100%', height: 100, backgroundColor: 'cyan' }}>
+              <Link className="VideoList__link" to={'/watch?v=/sample5.mp4&id=2&cast=Test2,TTTTT'}>List {value}</Link>
+            </div>
+          ))}
         </div>
       </div>
     );
