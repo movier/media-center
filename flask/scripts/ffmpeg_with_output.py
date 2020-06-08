7300da5eeb7a7a5b8f17e1029f43a33d2647410f
Oliver <olimovier@gmail.com>
2020-06-08 18:04:03 +0800

    Write one script for ffmpeg with output

---
+++ flask/scripts/ffmpeg_with_output.py
@@ -0,0 +1,20 @@
+import subprocess
+
+def runBash(command):
+    try:
+        output = subprocess.check_output(command, shell=True,
+                                     stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        print('Execution of "%s" failed!\n' % command)
+        sys.exit(1)
+
+    # Process output
+    for line in output.splitlines():
+        print('Line', line)
+
+def crop(start,end,input,output):
+	str = "ffmpeg -i " + input + " -ss  " + start + " -to " + end + " -c copy " + output
+	runBash(str)
+
+crop("00:00:00","00:00:04","sample.mp4","output.mp4")
+print('Done!')
