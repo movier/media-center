d9a88ecac07b8e36326caaed7b016b9fba55ecdb
Oliver <olimovier@gmail.com>
2021-04-06 10:54:47 +0800

    Refactor VideoList using React hooks

--- react/src/features/video/VideoList.js
+++ react/src/features/video/VideoList.js
@@ -1,78 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './VideoList.css';
 import { Link } from "react-router-dom";
 
-class VideoList extends React.Component {
+export default function VideoList(props) {
 
-  constructor(props) {
-    super(props);
-  
-    let initialData = [];
-    const data = localStorage.getItem('data');
-    if (data) {
-      initialData = JSON.parse(data);
-    }
-    this.state = {
-      data: initialData,
-      test: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-    };
+  let initialData = [];
+  const data = localStorage.getItem('data');
+  if (data) {
+    initialData = JSON.parse(data);
   }
+  
+  const [ videoList, setVideoList ] = useState(initialData);
 
-  componentDidMount() {
-    if (this.state.data.length > 0) return;
+  useEffect(() => {
+    if (videoList.length > 0) return;
     fetch('/api')
       .then(response => response.json())
-      .then(data => { 
+      .then(data => {
         console.log('response', data);
         if (data.has_error && data.error_code === 403) {
           console.log('error', data.error_message);
           const { remaining_seconds: remainingSeconds } = data.data;
-          this.props.history.replace('/remaining-time', { remainingSeconds });
+          props.history.replace('/remaining-time', { remainingSeconds });
         } else {
           localStorage.setItem('data', JSON.stringify(data));
-          this.setState({ data });
+          setVideoList(data);
         }
       });
-  }
+  }, []);
 
-  handleCheckUpdate = () => {
+  const handleCheckUpdate = () => {
     fetch('/api?is_check=true')
       .then(response => response.json())
       .then(data => {
         localStorage.setItem('data', JSON.stringify(data));
-        this.setState({ data });
+        setVideoList(data);
       });
   }
 
-  render() {
-    return (
-      <div>
-        <div style={{ marginTop: 16, textAlign: 'center' }}>
-          <button onClick={this.handleCheckUpdate}>检查更新</button>
-          <Link to="/cast">Cast</Link>
-        </div>
-        <div className="VideoList">
-          {this.state.data.map((value, index) => {
-            const { uri, poster_uri, title, id, cast } = value;
-            const castNames = cast.map(m => m.name).join(',');
-            return (
-              <div key={index}>
-                <Link className="VideoList__link" to={`/watch?v=${uri}&id=${id}&cast=${castNames}`}>
-                  <img src={poster_uri} alt={title} />
-                  <p className="VideoList__title">{title}</p>
-                </Link>
-              </div>
-            );
-          })}
-          {/* {this.state.test.map(value => (
-            <div key={value} style={{ width: '100%', height: 100, backgroundColor: 'cyan' }}>
-              <Link className="VideoList__link" to={'/watch?v=/sample5.mp4&id=2&cast=Test2,TTTTT'}>List {value}</Link>
-            </div>
-          ))} */}
-        </div>
+  return (
+    <div>
+      <div style={{ marginTop: 16, textAlign: 'center' }}>
+        <button onClick={handleCheckUpdate}>检查更新</button>
+        <Link to="/cast">Cast</Link>
       </div>
-    );
-  }
-}
+      <div className="VideoList">
+        {videoList.map((value, index) => {
+          const { uri, poster_uri, title, id, cast } = value;
+          const castNames = cast.map(m => m.name).join(',');
+          return (
+            <div key={index}>
+              <Link className="VideoList__link" to={`/watch?v=${uri}&id=${id}&cast=${castNames}`}>
+                <img src={poster_uri} alt={title} />
+                <p className="VideoList__title">{title}</p>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 
-export default VideoList;
+}
