4474dbaa7cd50877e63efe5e875c40e2d042b628
Oliver <olimovier@gmail.com>
2021-04-22 15:52:10 +0800

    DB: update uri and poster_uri saving absolute path

---
+++ flask/update_uri.py
@@ -0,0 +1,12 @@
+import os
+from manage import db, Media
+
+# base_path = "/mnt/sda4/data/kids"
+base_path = "/mnt/sda4/data/AI"
+
+for media in db.session.query(Media).all():
+  media.uri = base_path + media.uri
+  media.poster_uri = base_path + media.poster_uri
+  db.session.add(media)
+
+db.session.commit()
