b7ab33e57a10431534b4ae35d434777465ac31cd
Oliver <olimovier@gmail.com>
2021-04-23 17:35:07 +0800

    Update script

--- flask/add_column_media_type_and_size_to_media.py
+++ flask/add_column_media_type_and_size_to_media.py
@@ -1,13 +1,12 @@
 import os
 from manage import db, Media
 
-base_path = "/mnt/sda4/data/kids"
-# base_path = "/mnt/sda4/data/AI"
+# base_path = "/mnt/sda4/data/kids"
+base_path = "/mnt/sda4/data/AI"
 
 for media in db.session.query(Media).all():
-  if os.path.isfile(base_path + media.uri):
-    file_size = os.path.getsize(base_path + media.uri)
-    print(file_size)
+  if os.path.isfile(media.uri):
+    file_size = os.path.getsize(media.uri)
     media.size = file_size
     media.media_type = 1
     db.session.add(media)
--- flask/update_duration_and_datetime.py
+++ flask/update_duration_and_datetime.py
@@ -2,19 +2,8 @@ import os, sys, subprocess
 from manage import db, Media
 from datetime import datetime
 
-base_path = "/mnt/sda4/data/kids"
-# base_path = "/mnt/sda4/data/AI"
-
-def runBash(command):
-  try:
-    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-  except subprocess.CalledProcessError:
-    print('Execution of "%s" failed!\n' % command)
-    sys.exit(1)
-
-  # Process output
-  for line in output.splitlines():
-    print('Line', line)
+# base_path = "/mnt/sda4/data/kids"
+base_path = "/mnt/sda4/data/AI"
 
 def get_duration(filename):
   result = subprocess.run(
@@ -27,22 +16,19 @@ def get_duration(filename):
 def get_datetime(filename):
   result = subprocess.run(
     ["ffprobe", "-v", "quiet", "-select_streams", "v:0", "-show_entries", "stream_tags=creation_time", "-of", "default=noprint_wrappers=1:nokey=1", filename],
-    # ["ffprobe", "-v", "quiet", "-print_format", "json", "-show_entries", "stream=index,codec_type:stream_tags=creation_time:format_tags=creation_time", filename],
-    # ffprobe -v quiet input.mp4 -print_format json -show_entries stream=index,codec_type:stream_tags=creation_time:format_tags=creation_time
     stdout=subprocess.PIPE,
     stderr=subprocess.STDOUT,
   )
   return result.stdout.decode("utf-8").rstrip()
 
 for media in db.session.query(Media).all():
-  # print(media.uri)
-  get_video_info = "ffprobe -v error -show_format " + media.uri
-  duration = get_duration(media.uri)
-  print(duration)
-  media.duration = duration
-  datetime1 = get_datetime(media.uri)
-  print(datetime1)
-  media.datetime = datetime.strptime(datetime1, '%Y-%m-%dT%H:%M:%S.%fZ')
-  db.session.add(media)
+  if os.path.isfile(media.uri):
+    duration = get_duration(media.uri)
+    media.duration = duration
+
+    creation_datetime = get_datetime(media.uri)
+    media.datetime = datetime.strptime(creation_datetime, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+    db.session.add(media)
 
 db.session.commit()
