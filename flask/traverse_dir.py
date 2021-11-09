10f8e7e20db059d1efc88c6d10160390b61cbd48
Oliver <olimovier@gmail.com>
2021-11-08 21:37:31 -0400

    Clean code

--- flask/traverse_dir.py
+++ flask/traverse_dir.py
@@ -1,6 +1,7 @@
 from os import listdir
-from os.path import isfile, join, splitext, getmtime
+from os.path import isfile, join, splitext, getmtime, getsize
 from datetime import datetime
+from update_duration_and_datetime import get_datetime, get_duration
 
 mypath = "/mnt/sda4/data/kids" # dynamic
 
@@ -16,7 +17,26 @@ def traverse_dir(base_path):
                 poster_uri = "".join(root) + ".jpg"
                 mtimestamp = getmtime(path)
                 mdatetime = datetime.fromtimestamp(mtimestamp).isoformat()
-                print(dict(title=title, uri=uri, poster_uri=poster_uri, mtime=mdatetime))
+                file_size = getsize(path)
+
+                creation_datetime = get_datetime(path)
+                media_datetime = None
+                if creation_datetime:
+                    media_datetime = datetime.strptime(creation_datetime, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+                duration = get_duration(path)
+
+                print(dict(
+                    title=title,
+                    uri=uri,
+                    poster_uri=poster_uri,
+                    created_at=mdatetime,
+                    media_type=1,
+                    size=file_size,
+                    datetime=creation_datetime,
+                    duration=duration,
+                    filename=f,
+                ))
         else:
             traverse_dir(path)
 
--- flask/update_duration_and_datetime.py
+++ flask/update_duration_and_datetime.py
@@ -1,5 +1,5 @@
 import os, sys, subprocess
-from manage import db, Media
+# from manage import db, Media
 from datetime import datetime
 
 base_path = "/mnt/sda4/data/kids"
@@ -21,15 +21,15 @@ def get_datetime(filename):
   )
   return result.stdout.decode("utf-8").rstrip()
 
-for media in db.session.query(Media).all():
-  if os.path.isfile(media.uri):
-    duration = get_duration(media.uri)
-    media.duration = duration
+# for media in db.session.query(Media).all():
+#   if os.path.isfile(media.uri):
+#     duration = get_duration(media.uri)
+#     media.duration = duration
 
-    creation_datetime = get_datetime(media.uri)
-    if creation_datetime:
-      media.datetime = datetime.strptime(creation_datetime, '%Y-%m-%dT%H:%M:%S.%fZ')
+#     creation_datetime = get_datetime(media.uri)
+#     if creation_datetime:
+#       media.datetime = datetime.strptime(creation_datetime, '%Y-%m-%dT%H:%M:%S.%fZ')
 
-    db.session.add(media)
+#     db.session.add(media)
 
-db.session.commit()
+# db.session.commit()
