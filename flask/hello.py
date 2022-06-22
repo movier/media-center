55deb32078970a3378874c226722c4ae06ede88d
Oliver <olimovier@gmail.com>
2022-06-22 20:17:28 +0800

    BE: upload file

--- flask/hello.py
+++ flask/hello.py
@@ -1,6 +1,6 @@
 import os, shutil
 from datetime import datetime
-from flask import Flask, request, g
+from flask import Flask, request, g, flash, redirect
 from flask_restful import Resource, Api, fields, reqparse, abort, marshal
 # from database import db_session, db_session2
 from manage import Media, Shot, People, AndroidRelease
@@ -15,8 +15,45 @@ from sqlalchemy.ext.declarative import declarative_base
 from math import floor
 from update_duration_and_datetime import get_datetime, get_duration
 from traverse_dir import fini
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/mnt/sda4/data/AI'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect('/')
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
 CORS(app)
 api = Api(app)
 
