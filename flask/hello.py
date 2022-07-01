7815be4634175c60f652da1e2bca9134cdef06dd
Oliver <olimovier@gmail.com>
2022-07-01 23:24:47 +0800

    BE: refactor code

--- flask/hello.py
+++ flask/hello.py
@@ -29,33 +29,6 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#             flash('No file part')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # if user does not select file, browser also
-#         # submit an empty part without filename
-#         if file.filename == '':
-#             flash('No selected file')
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#             return redirect('/')
-#     return '''
-#     <!doctype html>
-#     <title>Upload new File</title>
-#     <h1>Upload new File</h1>
-#     <form method=post enctype=multipart/form-data>
-#       <input type=file name=file>
-#       <input type=submit value=Upload>
-#     </form>
-#     '''
-
 CORS(app)
 api = Api(app)
 
@@ -296,6 +269,24 @@ class ThumbnailController(Resource):
     generate_thumbnail(media.uri, media.poster_uri, screenshot_time)
     return '', 200
 
+class MediaList(Resource):
+  def post(self):
+    # check if the post request has the file part
+    if 'file' not in request.files:
+      # flash('No file part')
+      return 'No file part', 400 
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+      return 'No selected file', 400 
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      return '', 201
+    return '', 500
+
+api.add_resource(MediaList, '/media')
 api.add_resource(ThumbnailController, '/thumbnail/<media_id>')
 api.add_resource(FFmpegController, '/ffmpeg')
 api.add_resource(MediaController, '/media/<media_id>')
@@ -303,42 +294,6 @@ api.add_resource(ShotList, '/shots')
 api.add_resource(CastController, '/cast')
 api.add_resource(AndroidReleaseController, '/android')
 api.add_resource(HelloWorld, '/')
-
-@app.route('/upload', methods=['POST'])
-def upload_media():
-  # check if the post request has the file part
-  if 'file' not in request.files:
-    # flash('No file part')
-    return 'No file part', 400 
-  file = request.files['file']
-  # if user does not select file, browser also
-  # submit an empty part without filename
-  if file.filename == '':
-    return 'No selected file', 400 
-  if file and allowed_file(file.filename):
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return '', 201
-  return '', 500
-
-# @app.route('/generate_thumbnail/<media_id>')
-# def generate_thumbnail(media_id):
-#   media = abort_if_video_doesnt_exist(media_id)
-#   return marshal(media, resource_fields), 200
-  # # check if the post request has the file part
-  # if 'file' not in request.files:
-  #   # flash('No file part')
-  #   return 'No file part', 400 
-  # file = request.files['file']
-  # # if user does not select file, browser also
-  # # submit an empty part without filename
-  # if file.filename == '':
-  #   return 'No selected file', 400 
-  # if file and allowed_file(file.filename):
-  #   filename = secure_filename(file.filename)
-  #   file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-  #   return '', 201
-  # return '', 500
 
 @app.before_request
 def open_db_session():
