e0284ee6b46b5318b334b94c66fa3bd4e0338da0
Oliver <olimovier@gmail.com>
2022-07-06 22:58:11 +0800

    FE: work on upload file

--- react/src/features/video/MediaList.js
+++ react/src/features/video/MediaList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   saveVideoListData,
@@ -12,6 +12,7 @@ export default function MediaList(props) {
 
   const videoListData = useSelector(selectVideoList);
   const dispatch = useDispatch();
+  const [selectedFile, setSelectedFile] = useState(null);
 
   useEffect(() => {
     if (videoListData.length > 0) return;
@@ -37,10 +38,26 @@ export default function MediaList(props) {
       });
   }
 
+  const onFileChange = event => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  function onFileUpload() {
+    const formData = new FormData();
+    formData.append(
+      "myFile",
+      selectedFile,
+      selectedFile.name
+    );
+    // Send formData object
+    // axios.post("api/uploadfile", formData);
+  };
+
   return (
     <div>
       <div style={{ margin: 16, textAlign: 'center' }}>
-        <span style={{ cursor: 'pointer' }} className="material-symbols-outlined">file_upload</span>
+        <input type="file" onChange={onFileChange} />
+        <span style={{ cursor: 'pointer' }} className="material-symbols-outlined" onClick={onFileUpload}>file_upload</span>
         <span style={{ margin: '0 20px', cursor: 'pointer' }} className="material-symbols-outlined" onClick={handleCheckUpdate}>sync</span>
         <Link to="/cast"><span style={{ color: 'white' }}  className="material-symbols-outlined">familiar_face_and_zone</span></Link>
       </div>
